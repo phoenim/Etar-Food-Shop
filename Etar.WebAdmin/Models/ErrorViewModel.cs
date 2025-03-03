5edfef4958744b6e7523532d8524ca7cf68968cb
bikaar <nima.268183@gmail.com>
2025-03-03 21:22:18 +0330

    Add project files.

---
+++ Etar.WebAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Etar.WebAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
